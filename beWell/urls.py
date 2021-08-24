7fdd3bf856b24f51b6fdf2006f873eb8cce94e89
leocassiosilva <leocassiosilva1234@gmail.com>
2021-08-23 21:28:20 -0300

    tentando deploy

--- beWell/urls.py
+++ beWell/urls.py
@@ -20,6 +20,10 @@ from rest_framework import routers
 from accounts.views import UsuarioViewSet
 from podcasts.views import PodcastViewSet
 from videos.views import VideoViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'podcasts', PodcastViewSet)
@@ -29,8 +33,12 @@ router.register(r'videos', VideoViewSet)
 
 
 urlpatterns = [
+
+    
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls'))
 
 ]
