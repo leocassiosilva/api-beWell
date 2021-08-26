55f7b9ba3cde70099246e04d97724a157908a397
leocassiosilva <leocassiosilva1234@gmail.com>
2021-08-25 22:09:22 -0300

    create user

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth.hashers import make_password
 from accounts.models import CustomUsuario
 
 
@@ -7,3 +7,7 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUsuario
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UsuarioSerializer, self).create(validated_data)
