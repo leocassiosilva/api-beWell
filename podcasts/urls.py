309480d4cd6f1f90a35c099e547ca64e1110bd99
leocassiosilva <leocassiosilva1234@gmail.com>
2021-08-29 08:50:49 -0300

    view para listar podcast do user

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-   PodcastListView
+   PodcastListView,
+   PodcastListViewUser
 )
 
 
 urlpatterns = [
    path('podcast/',PodcastListView.as_view(),name='podcast'),
+   path('meus-podcasts/', PodcastListViewUser.as_view(), name='meus-podecasts')
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,13 +1,26 @@
 from rest_framework import viewsets
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.mixins import ListModelMixin
 from .models import Podcast
 from podcasts.serializer import PodcastSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class PodcastViewSet(viewsets.ModelViewSet):
     queryset = Podcast.objects.all()
     serializer_class = PodcastSerializer
+
+
+#List view para todos os usuarios    
+class PodcastListViewUser(ListAPIView):
+    permission_classes = (IsAuthenticated, )
+    #queryset = CustomUsuario.objects.all()
+    serializer_class = PodcastSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        print(user)
+        return Podcast.objects.order_by('nome').filter(id_usuario=self.request.user)
 
 
 #List view para todos os usuarios
