b8941d51ba158a2a2e59f6ee56fc7c7194a88335
leocassiosilva <leocassiosilva1234@gmail.com>
2021-09-01 22:26:11 -0300

    mudando tempo do jwt

--- beWell/settings.py
+++ beWell/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from datetime import datetime
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
